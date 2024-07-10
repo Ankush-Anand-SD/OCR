24ebf0f464cbd9b014b7e27417f39390c438bed5
Ankush Anand <ankush.anand@verifacts.co.in>
2024-07-10 18:23:37 +0530

    console added

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const tesseract = require('tesseract.js')
 // const outputPath = 'C:/Users/Ankush/Desktop/pdf/tmp';
 tesseract.recognize('C:/Users/Ankush/Desktop/pdf/tmp/page-1.jpg', 'eng', { logger: e => console.log((e)) })
     .then(out => console.log(out))
+console.log("code ended with status one :) ")
